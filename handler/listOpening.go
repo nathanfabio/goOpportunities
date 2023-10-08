f90716d2c481f462afffafa1d9631fa0c679158f
Nathan Fabio <nathan_3_m@hotmail.com>
2023-10-08 20:04:08 -0300

    docs: add documentation for List openings

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -7,7 +7,18 @@ import (
 	"github.com/nathanfabio/goOpportunities/schemas"
 )
 
-func ListOpeningHandler(ctx *gin.Context) {
+
+// @BasePath /api/v1
+
+// @Summary List Opening
+// @Description List all job openings
+// @Tags openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
+func ListOpeningsHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 	
 	if err := db.Find(&openings).Error; err != nil {
